cee62a5e8c8c479d92af79edd766004959040434
Aurcereal <aidannn@seas.upenn.edu>
2025-08-25 17:28:44 -0400

    Fall Scene Pretty Done

--- Assets/Env/FallEnvPhysics.hlsl
+++ Assets/Env/FallEnvPhysics.hlsl
@@ -1,4 +1,5 @@
 float sdEnv(float3 p) {
-    float dFloor = sdBox(p - float3(0., -6.5, 0.), float3(120., 0.1, 120.));
-    return dFloor;
+    float dFloor = sdBox(p - float3(0., -1.5, 0.), float3(120., 0.1, 120.));
+    float dWasher = sdWasher(p, float3(9., 30., 0.9));
+    return min(dFloor, dWasher);
 }
